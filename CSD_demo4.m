bb5017e8069a37149a3f709b29479f211040e38a
yzhizai <duansf@ihep.ac.cn>
2016-03-24 22:16:12 +0800

    modify as instruction

--- CSD_demo4.m
+++ CSD_demo4.m
@@ -1,20 +1,15 @@
+function CSD_demo4
+%This is the most useful demo to test CSD method.
 D1 = diag([1.7, 0.2, 0.2]);
 D2 = diag([0.2, 0.2, 1.7]);
 
-diff_ori = load('Grad_dirs_60.txt');
-F = zeros(size(diff_ori, 1), 1);
-for aa = 1:size(diff_ori, 1)
-    k = diff_ori(aa, :);
-    F(aa) = -1/1000*log(0.5*(exp(-k*D1*k') + exp(-k*D2*k')));
-end
-x = diff_ori(:,1);
-y = diff_ori(:,2);
-z = diff_ori(:,3);
+myfunc = @(k) -1/1000*log(0.5*(exp(-k*D1*k') + exp(-k*D2*k')));
 
-[Phi, Theta, ~] = cart2sph(x, y, z);
-Ori = [pi/2 - Theta, Phi];
-Q = getQMatrix(Ori, 10);
+filename1 = 'Grad_dirs_60.txt';
+filename2 = 'Grad_dirs_300.txt';
 
+[Q, F] = getMatrix(filename1, myfunc);
+P = getMatrix(filename2);
 M = pinv(Q'*Q)*Q';
 f0 = M*F;
 
@@ -27,23 +22,48 @@ R(5, 5) = 0.0016;
 R(6, 6) = 0.0016;
 
 A = Q*R;
-
+  
 lambda = 1;
 fi = f0;
-u = Q*fi;
-L1 = Q;
+fi(16:end) = 0;
+u = P*fi;
+L1 = P;
 L0 = ones(size(L1));
 L1(u < mean(u), :) = 0;
-aa  = 0;
 while sum(sum((L1 - L0).^2)) ~= 0
     L = lambda*L1;
     fi = pinv(A'*A + L'*L)*A'*F;
     save('test.mat', 'fi');
     L0 = L1;
-    u = Q*fi;
-    L1 = Q;
-    L1(find(u < mean(u)), :) = 0;
+    u = P*fi;
+    L1 = P;
+    L1(find(u >= 0.1*mean(u)), :) = 0;
 end
+C = fi;
 
+save('exchange.mat', 'C', 'f0');
 
-save('exchange.mat', 'C', 'F', 'Ori');
+function varargout = getMatrix(varargin)
+
+%Usage :[Q, F] = getMatrix(filename, func);
+%       Q = getMatrix(filename);
+diff_ori = load(varargin{1});
+x = diff_ori(:,1);
+y = diff_ori(:,2);
+z = diff_ori(:,3);
+
+[Phi, Theta, ~] = cart2sph(x, y, z);
+Ori = [pi/2 - Theta, Phi];
+Q = getQMatrix(Ori, 10);
+
+varargout{1} = Q;
+if  nargin < 2
+    return;
+end
+func = varargin{2};
+F = zeros(size(diff_ori, 1), 1);
+for aa = 1:size(diff_ori, 1)
+    k = diff_ori(aa, :);
+    F(aa) = func(k);
+end
+varargout{2} = F;
